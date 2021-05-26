92dff64b072135d22707f8af45c5f7ace0c0610c
Singh Faujdar <Mrityunjay.SinghFaujdar@ellucian.com>
2021-05-26 16:15:11 +0530

    Register a User, Encrypt Password, Return Token - JWT

--- config/db.js
+++ config/db.js
@@ -1,16 +1,20 @@
 const mongoose = require("mongoose");
-const config = require('config');
+const config = require("config");
 
 const databaseURL = config.get("mongoURI");
 
-const connectToDB = async () => { 
+const connectToDB = async () => {
     try {
-        await mongoose.connect(databaseURL, { useNewUrlParser: true, useUnifiedTopology: true });
-        console.log("MongoDB Connected...")        
-    } catch (err) { 
+        await mongoose.connect(databaseURL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        });
+        console.log("MongoDB Connected...");
+    } catch (err) {
         console.error(err.message);
         process.exit(1);
     }
-}
+};
 
 module.exports = connectToDB;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,86 @@
 const express = require("express");
 const router = express.Router();
+const {check, validationResult} = require("express-validator");
+const User = require("../../models/User");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 // @route       Post api/users/register
 // @desc        Test Route
 // @access      Public
-router.post("/", (req, res) => {
-    console.log(req.body);
-    res.send("Usr Route");
-});
+router.post(
+    "/",
+    [
+        check("name", "Name is required").not().isEmpty(),
+        check("email", "Please include a valid email").isEmail(),
+        check(
+            "password",
+            "Please enter minimum 6 characters for password."
+        ).isLength({min: 3}),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            //bad request
+            return res.status(400).json({errors: errors.array()});
+        }
+
+        const {name, email, password} = req.body;
+        try {
+            let user = await User.findOne({email});
+
+            if (user) {
+                //User exist
+                return res
+                    .status(400)
+                    .json({errors: [{msg: "User already exists."}]});
+            }
+
+            const avatar = gravatar.url(email, {
+                s: "200",
+                r: "pg",
+                d: "mm",
+            });
+
+            user = new User({
+                name,
+                email,
+                avatar,
+                password,
+            });
+
+            //encrypt the passsword
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+
+            //Return a JSON webtoken
+            const payload = {
+                user: {
+                    id: user.id,
+                },
+            };
+
+            const jwtSecret = config.get("jwtSecret");
+            jwt.sign(
+                payload,
+                jwtSecret,
+                {
+                    expiresIn: 360000,
+                },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({token});
+                }
+            );
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Server Error....");
+        }
+    }
+);
 
 module.exports = router;
